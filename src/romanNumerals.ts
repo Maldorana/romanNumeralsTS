72cd4a6e805d7133f975757e9ff940ddc350be62
Maldorana <maldorana@gmail.com>
2021-11-23 19:42:50 +0000

    Third test passing for 3 and refacotring

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,4 +1,4 @@
 export function romanNumber(number: number): string {
-  if (number === 2) return 'II';
-  return 'I';
+  if (number > 0) return 'I' + romanNumber(number - 1);
+  return '';
 }
--- test/romanNumerals.test.ts
+++ test/romanNumerals.test.ts
@@ -7,4 +7,7 @@ describe('romanNumerals tests', () => {
   it('should return II when sending 2', () => {
     expect(romanNumber(2)).toBe('II');
   });
+  it('should return III when sending 3', () => {
+    expect(romanNumber(3)).toBe('III');
+  });
 });
