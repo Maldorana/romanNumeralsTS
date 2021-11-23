c67f26daf3d93d86c987f7d4cc0ebc1047d38829
Maldorana <maldorana@gmail.com>
2021-11-23 19:35:23 +0000

    Second test passing for 2

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,3 +1,4 @@
 export function romanNumber(number: number): string {
+  if (number === 2) return 'II';
   return 'I';
 }
--- test/romanNumerals.test.ts
+++ test/romanNumerals.test.ts
@@ -4,4 +4,7 @@ describe('romanNumerals tests', () => {
   it('should return I when sending 1', () => {
     expect(romanNumber(1)).toBe('I');
   });
+  it('should return II when sending 2', () => {
+    expect(romanNumber(2)).toBe('II');
+  });
 });
